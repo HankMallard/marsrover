85844fd121a99258ff0ec871b2aa8fc573e01272
howardl01@live.ca <howardl01@live.ca>
2016-06-04 17:22:32 -0400

    All directional rover

--- rover.rb
+++ rover.rb
@@ -2,15 +2,58 @@ class Rover
 
   attr_accessor :direction, :position_x, :position_y
 
+  # def initialize(name, direction , position_x, position_y)
+  #   @name = name
+  #   @direction = direction
+  #   @position_x = position_x
+  #   @position_y = position_x
+
+  # end
+
   def direction(turn_direction)
     if turn_direction == "L" && @direction == "North"
-      @direction = "W"
+      @direction = "West"
+
+    elsif turn_direction == "L" && @direction == "West"
+      @direction = "South"
+
+    elsif turn_direction == "L" && @direction == "South"
+      @direction = "East"
+
+    elsif turn_direction == "L" && @direction == "East"
+      @direction = "North"
+
+    elsif turn_direction == "R" && @direction == "North"
+      @direction = "East"
+
+    elsif turn_direction == "R" && @direction == "East"
+      @direction = "South"
+
+    elsif turn_direction == "R" && @direction == "South"
+      @direction = "West"
+
+    elsif turn_direction == "R" && @direction == "West"
+      @direction = "North"
+
     end
   end
 
   def move
-    if @direction== "W"
+    if @direction== "West"
       @position_x -= 1
+      @position_y = 0
+
+    elsif @direction== "North"
+      @position_x = 0
+      @position_y += 1
+
+    elsif @direction== "East"
+      @position_x += 1
+      @position_y = 0
+
+    elsif @direction== "South"
+      @position_x = 0
+      @position_y -= 1
 
     end
   end
@@ -18,11 +61,25 @@ class Rover
   def status
     return "The Rover is currently at #{@position_x},#{@position_y} and facing #{@direction}!"
   end
+
 end
+
 rover1 = Rover.new
-rover1.position_x = 0
-rover1.position_y = 0
-rover1.direction = "North"
+rover1.position_x = 1
+rover1.position_y = 1
+rover1.direction = "East"
+puts rover1.status
+rover1.direction("R")
+puts rover1.status
+rover1.move
+puts rover1.status
+rover1.direction("L")
+puts rover1.status
+rover1.move
+puts rover1.status
+rover1.direction("R")
+puts rover1.status
+rover1.move
 puts rover1.status
 rover1.direction("L")
 puts rover1.status
